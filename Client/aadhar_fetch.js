673616d23acfe7ecd995379426a35165d027c5a9
chetana-1929 <mca.1929@unigoa.ac.in>
2021-02-14 22:49:40 +0530

    changes

--- Client/aadhar_fetch.js
+++ Client/aadhar_fetch.js
@@ -1,3 +1,12 @@
+
+var aadharnumber = document.getElementById("aadharnumber");
+var name = document.getElementById("name");
+var address = document.getElementById("address");
+var dob = document.getElementById("dob");
+var gender = document.getElementById("gender");
+var phone = document.getElementById("phone");
+ 
+
 function checkAadhar(aadharCardNumber) {
   if (aadharCardNumber.length == 12) {
   }
