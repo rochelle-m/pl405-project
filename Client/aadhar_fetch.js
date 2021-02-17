528dc706eae67049838d2e28d86d3327beea3b74
rochelle-m <mca.1930@unigoa.ac.in>
2021-02-17 18:02:02 +0530

    Minor changes

--- Client/aadhar_fetch.js
+++ Client/aadhar_fetch.js
@@ -3,33 +3,28 @@ async function checkAadhar(aadharCardNumber) {
   var _name = document.getElementById("name");
   var _address = document.getElementById("address");
   var _dob = document.getElementById("dob");
- // var _gender = document.getElementById("gender");
-  var _gender = document.getElementsByName("gender")
+  // var _gender = document.getElementById("gender");
+  var _gender = document.getElementsByName("gender");
   var _phone = document.getElementById("phone");
   if (aadharCardNumber.length == 12) {
     console.log(aadharCardNumber);
     const res = await fetch(`http://127.0.0.1:8001/find/${aadharCardNumber}/`);
-    const details = await res.json();
+    const person = await res.json();
 
-    _name.value =
-      details.first_name + " " + details.middle_name + " " + details.last_name;
+    const { first_name, middle_name, last_name } = person;
+    _name.value = first_name + " " + middle_name + " " + last_name;
     _name.disabled = true;
 
-    _address.value = details.address + " - " + details.pincode;
+    _address.value = person.address + " - " + person.pincode;
 
-    _phone.value = details.contact;
+    _phone.value = person.contact;
 
-    
-    for (var i=0; i<_gender.length; i++) {
-      if (_gender[i].getAttribute('value') == details.gender.toLowerCase() ) {
+    // * alternately, document.getElementById(person.gender.toLowerCase()).checked = true;
+
+    for (var i = 0; i < _gender.length; i++) {
+      if (_gender[i].getAttribute("value") == person.gender.toLowerCase()) {
         _gender[i].checked = true;
       }
-      
     }
-
-
-
-
-
   }
 }
