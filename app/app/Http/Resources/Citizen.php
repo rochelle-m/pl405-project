250f2eda8aa3337271af070aefe234ae37a8fd03
rochelle-m <mca.1930@unigoa.ac.in>
2021-02-18 10:45:27 +0530

    Create citizen resource

--- app/app/Http/Resources/Citizen.php
+++ app/app/Http/Resources/Citizen.php
@@ -14,6 +14,18 @@ class Citizen extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'first_name' => $this->first_name,
+            'middle_name' => $this->middle_name,
+            'last_name' => $this->last_name,
+            'gender' => $this->gender,
+            'date_of_birth' => $this->date_of_birth,
+            'aadhar_no' => $this->aadhar_no,
+            'email'=> $this->email,
+            'pincode'=> $this->pincode,
+            'password'=> $this->password,
+            'phone_no' => $this->phone_no,
+            'street' => $this->street
+        ];
     }
 }
