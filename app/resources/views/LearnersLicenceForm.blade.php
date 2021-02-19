4232bbe94035a7bd7653a8a31c2389d6424a328d
rochelle-m <mca.1930@unigoa.ac.in>
2021-02-20 02:26:38 +0530

    Fix request body

--- app/resources/views/LearnersLicenceForm.blade.php
+++ app/resources/views/LearnersLicenceForm.blade.php
@@ -21,14 +21,13 @@
                 padding: 1rem;
             }
         </style>
-        
     </head>
     <body>
         <div class="container">
             <h1 style="text-align: center" class="title">Registration Form</h1>
             <div class="col-lg-12 well">
                 <div class="row">
-                    <form method="post" action="success.php">
+                    <form name="registration">
                         <div class="col-sm-12">
                             <div class="row">
                                 <div class="form-group">
@@ -59,6 +58,20 @@
                                     />
                                 </div>
                             </div>
+                            <div class="row">
+                                <div class="form-group">
+                                    <label>Email</label>
+                                    <input
+                                        type="email"
+                                        id="email"
+                                        name="email"
+                                        placeholder="Enter Name Here.."
+                                        class="form-control"
+                                        required=""
+                                    />
+                                </div>
+                            </div>
+
                             <div class="row">
                                 <div class="form-group">
                                     <label>Address</label>
@@ -175,7 +188,7 @@
                                 </div>
                             </div>
 
-                            <div class="row">
+                            <!-- <div class="row">
                                 <div class="form-group">
                                     <label>Password</label>
                                     <input
@@ -203,56 +216,106 @@
                                         required=""
                                     />
                                 </div>
-                            </div>
+                            </div> -->
                             <div class="text-center">
                                 <input
                                     type="submit"
                                     name="submit"
                                     value="Register"
                                     class="btn btn-lg btn-info"
+                                    id="btn-submit"
                                 />
                             </div>
-                        </div>  
+                        </div>
                     </form>
                 </div>
             </div>
         </div>
 
-        
         <script>
             async function checkAadhar(aadharCardNumber) {
-            var _name = document.getElementById("name");
-            var _address = document.getElementById("address");
-            var _dob = document.getElementById("dob");
-            var _gender = document.getElementsByName("gender");
-            var _phone = document.getElementById("phone");
-            if (aadharCardNumber.length == 12) {
-                console.log(aadharCardNumber);
-                const res = await fetch(`http://127.0.0.1:8001/find/${aadharCardNumber}/`);
-                const person = await res.json();
+                var _name = document.getElementById("name");
+                var _aadhar_no = document.getElementById("aadharnumber");
+                var _address = document.getElementById("address");
+                var _email = document.getElementById("email");
+                var _dob = document.getElementById("dob");
+                var _gender = document.getElementsByName("gender");
+                var _phone = document.getElementById("phone");
+                if (aadharCardNumber.length == 12) {
+                    console.log(aadharCardNumber);
+                    const res = await fetch(
+                        `http://127.0.0.1:8001/find/${aadharCardNumber}/`
+                    );
+                    const person = await res.json();
 
-                const { first_name, middle_name, last_name } = person;
-                _name.value = first_name + " " + middle_name + " " + last_name;
-                _name.disabled = true;
+                    const { first_name, middle_name, last_name } = person;
+                    _name.value =
+                        first_name + " " + middle_name + " " + last_name;
+                    _name.disabled = true;
 
-                _address.value = person.address + " - " + person.pincode;
+                    _address.value = person.address + " - " + person.pincode;
 
-                _phone.value = person.contact;
+                    _phone.value = person.contact;
+                    console.log(
+                        new Date(person.dob)
+                            .toISOString()
+                            .slice(0, 19)
+                            .replace("T", " ")
+                    );
 
-                _dob.valueAsDate = new Date(person.dob);
-                _dob.disabled = true;
+                    _dob.valueAsDate = new Date(person.dob);
+                    _dob.disabled = true;
 
-                // * alternately, document.getElementById(person.gender.toLowerCase()).checked = true;
+                    // * alternately, document.getElementById(person.gender.toLowerCase()).checked = true;
 
-                for (var i = 0; i < _gender.length; i++) {
-                if (_gender[i].getAttribute("value") == person.gender.toLowerCase()) {
-                    _gender[i].checked = true;
-                }
-                _gender[i].disabled = true;
+                    for (var i = 0; i < _gender.length; i++) {
+                        if (
+                            _gender[i].getAttribute("value") ==
+                            person.gender.toLowerCase()
+                        ) {
+                            _gender[i].checked = true;
+                        }
+                        _gender[i].disabled = true;
+                    }
+
+                    document
+                        .getElementById("btn-submit")
+                        .addEventListener("click", async (event) => {
+                            event.preventDefault();
+                            let personObj = {
+                                first_name: person.first_name,
+                                middle_name,
+                                last_name,
+                                gender: document.querySelector(
+                                    'input[name="gender"]:checked'
+                                ).value,
+                                date_of_birth: new Date(person.dob)
+                                    .toISOString()
+                                    .slice(0, 19)
+                                    .replace("T", " "),
+                                aadhar_no: _aadhar_no.value,
+                                email: _email.value,
+                                pincode: person.pincode,
+                                password: "999",
+                                phone_number: _phone.value,
+                                street: person.address,
+                            };
+                            console.log(personObj);
+
+                            const resp = await fetch(
+                                "http://127.0.0.1:8000/api/citizen",
+                                {
+                                    headers: {
+                                        "content-type": "application/json",
+                                    },
+                                    body: JSON.stringify(personObj),
+                                    method: "POST",
+                                }
+                            );
+                            console.log(resp);
+                        });
                 }
             }
-            }
-
         </script>
     </body>
 </html>
