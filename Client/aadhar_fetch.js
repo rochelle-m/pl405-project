d058650682f834527c15a2ea2dab786dc686ac4f
rochelle-m <70574900+rochelle-m@users.noreply.github.com>
2021-02-23 23:04:47 +0530

    Add validations for license type

--- Client/aadhar_fetch.js
+++ Client/aadhar_fetch.js
@@ -47,5 +47,15 @@ $(document).ready(function () {
     $("#hdncheckbox").val(JSON.stringify(tmp));
     // $("#result").text($("#hdncheckbox").val());
     console.log($("#hdncheckbox").val());
+
+    let wGChecked = tmp.some(p => p == "MCWG")
+    let wOGChecked = tmp.some(p => p == "MCWOG")
+
+    let warning = document.getElementById("warning")
+    warning.style.color = "orange"
+    if(wGChecked && wOGChecked)
+      warning.innerHTML = "You have checked MCWG and MCWOG? MCWG will be considered in your application"
+    else
+      warning.innerHTML = ""
   });
 });
