f28113d46e781ab0c3cd3162800e3498f4ba283a
chetana-1929 <mca.1929@unigoa.ac.in>
2021-03-01 14:22:01 +0530

    correction

--- app/resources/views/learners/payment.blade.php
+++ app/resources/views/learners/payment.blade.php
@@ -6,44 +6,44 @@
 <link href="{{asset('css/welcome.css')}}" rel="stylesheet">
 </head>
 
-    <body>
+     <body>
         
         <div class="sidenav">
             <a href="http://localhost:8000/register">Apply Learner License</a>
-			<a href="http://localhost:8000/LLRenew">Renew lerning license</a>
+      <a href="http://localhost:8000/LLRenew">Renew lerning license</a>
             <a href="#DrivingLicense">Apply Driving License</a>
             <a href="#status">Application Status</a>
             <a href="#resources">Study Material</a>
         </div>
 
-        <div class="main" >
+        <div class="header" >
             <h2>Road Transport Service Application</h2>
         
         </div>
-		
+    
         <div>
 
         </div>
-		
-		  <?php 
+    
+      <?php 
            $d=date("Y-m-d", strtotime("+1 week"));
-	       $dayofweek = date('w', strtotime($d));
-	   if($dayofweek == 'Sunday')
-		   $d = date("Y-m-d", strtotime("+1 day"));?>
+         $dayofweek = date('w', strtotime($d));
+     if($dayofweek == 'Sunday')
+       $d = date("Y-m-d", strtotime("+1 day"));?>
   
-		
+    
 <div class="col-md-10">
-	<table border ="1" cellpadding="10" cellspacing="5" align="center">
+  <table border ="1" cellpadding="10" cellspacing="5" align="center">
 <tr>
   <td align = "center" colspan="2" ><b>EXAM DETAILS</b></td>
-</tr>		
-		<tr>
+</tr>   
+    <tr>
   <td>Exam Date</td>
   <td><?php echo $d ?></td>
 </tr>
 <tr>
 
-</div>	
+</div>  
 <script>
 
 
@@ -59,25 +59,26 @@
   }
 
 </script>
-
+<br><br>
 <div class="normal " style="margin-left: 200px">
-{{-- I declare that I am 18 years old. --}}
+  I state that I am 18 years old. 
 <p>
-I am Indian
+I hold an Indian nationality.
 <p>
-I am physially fit to drive a vehicle.
+I am physically fit to ride/drive a vehicle.
 <p>
 
-<input type="text"  name="location"><br><br>
+
   <label for="location">location:-</label>
+  <input type="text"  name="location"><br>
 <p>
 <?php echo "Today is " . date("Y/m/d") . "<br>";?>
 
 
 <form ... onsubmit="return checkForm(this);">
-...
+ 
 <p><input type="checkbox" required name="terms"> I accept the <u>Terms and Conditions</u></p>
-<p><input type="submit"></p>
+<p><input id="submit" type="submit"></p>
 </form>
 <div class="razorpay-embed-btn" data-url="https://pages.razorpay.com/pl_GDHySC8xb8V2TZ/view" data-text="Pay Now" data-color="#528FF0" data-size="large">
   <script>
