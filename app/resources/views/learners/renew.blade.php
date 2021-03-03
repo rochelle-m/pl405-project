760f4a7c7ebe933c6dd1f1e0db8b3f123057864b
rochelle-m <70574900+rochelle-m@users.noreply.github.com>
2021-03-03 19:17:40 +0530

    fixes

--- app/resources/views/learners/renew.blade.php
+++ app/resources/views/learners/renew.blade.php
@@ -4,69 +4,14 @@
 <title>Road Transport Services</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="{{ asset('css/welcome.css') }}">  
- <style>
-     body {
-        font-family: "Lato", sans-serif;
-    }
 
-    .sidenav {
-        height: 100%;
-        width: 160px;
-        position: fixed;
-        z-index: 1;
-        top: 0;
-        left: 0;
-        background-color: #111;
-        overflow-x: hidden;
-        padding-top: 20px;
-    }
-
-    .sidenav a {
-        padding: 6px 8px 6px 16px;
-        text-decoration: none;
-        font-size: 20px;
-        color: #818181;
-        display: block;
-    }
-
-    .sidenav a:hover {
-        color: #f1f1f1;
-    }
-
-    .main {
-        margin-left: 160px; /* Same as the width of the sidenav */
-        font-size: 28px; /* Increased text to enable scrolling */
-        padding: 0px 10px;
-    }
-
-    @media screen and (max-height: 450px) {
-        .sidenav {
-            padding-top: 15px;
-        }
-        .sidenav a {
-            font-size: 18px;
-        }
-    }
-	
-	
-			.previous {
-		  background-color: #f1f1f1;
-		  color: black;
-		}
-
-		.next {
-		  background-color: #4CAF50;
-		  color: white;
-		}
-
-</style>
 </head>
 
     <body>
         
         <div class="sidenav">
-            <a href="http://localhost:8000/register">Apply Learner License</a>
-			<a href="http://localhost:8000/LLRenew">Renew lerning license</a>
+            <a href="learners/register">Apply Learner License</a>
+			<a href="">Renew lerning license</a>
             <a href="#DrivingLicense">Apply Driving License</a>
             <a href="#status">Application Status</a>
             <a href="#resources">Study Material</a>
