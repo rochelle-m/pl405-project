c92ef6827b3fcbf430af2467357f7dcfdb460dc7
rochelle-m <70574900+rochelle-m@users.noreply.github.com>
2021-04-04 16:24:14 +0530

    Retrieve and display image based test questions

--- app/app/Http/Controllers/QuestionController.php
+++ app/app/Http/Controllers/QuestionController.php
@@ -11,18 +11,18 @@ class QuestionController extends Controller
 {
     public function get_questions(Request $request){
         $questions = $questions = DB::table('questions')->
-          select('id', 'question', 'option1','option2','option3','option4', 'correct')->
+          select('id', 'img', 'question', 'option1','option2','option3','option4')->
           where('level', 'E')->inRandomOrder()->limit(20)->
           get()->all();
         return view('learners.test.question', ["questions"=> $questions, "index" => 0]);
     }
 
     public function post_questions(Request $request){
-        
-        $types = Learner::where('aadhar_no', $request->aadhar_no)->pluck('type')->all();;   
+        $types = Learner::where('aadhar_no', $request->aadhar_no)->pluck('type')->all();
+        array_push($types,"ALL");   
       
         $questions = DB::table('questions')->
-          select('id', 'question', 'option1','option2','option3','option4', 'correct')->
+          select('id', 'img', 'question', 'option1','option2','option3','option4')->
           where('level', 'D')->inRandomOrder()->limit(20)->
           whereIn('category', explode (', ',implode(", ",$types)))->get()->all();
        
--- app/resources/views/learners/test/question.blade.php
+++ app/resources/views/learners/test/question.blade.php
@@ -9,7 +9,11 @@
             <ul class="question">
                 <input type="text" value={{$questions[$index]->id}} name="id" hidden>
                 <input type="text" value={{$index + 1}}  name="index" hidden>
-                <img src={{$questions[$index]->img ?? ''}}  alt="" hidden>
+
+                @if ($questions[$index]->img != '?')
+                    <img src={{$questions[$index]->img ?? ''}}  alt="sign" style="width:20%;">
+                @endif
+
                 <li><input type="radio" name="answer" value="1">{{$questions[$index]->option1}}</li>
                 <li><input type="radio" name="answer" value="2">{{$questions[$index]->option2}}</li>
                 <li><input type="radio" name="answer" value="3">{{$questions[$index]->option3}}</li>
@@ -21,7 +25,11 @@
     
             <div class="controls">
                 <button class="skip" name="skip">Skip</button>
-                <button class="next" name="next"">Next</button>
+                @if ($index != 20)
+                    <button class="next" name="next">Next</button>       
+                @else
+                    <button class="next" name="next">Finish</button>           
+                @endif
             </div>
         </form>
     </div>
