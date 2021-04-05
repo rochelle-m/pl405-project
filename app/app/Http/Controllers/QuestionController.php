3e973f3e22d35429ef7dcd1ab79060e1caec4a41
rochelle-m <70574900+rochelle-m@users.noreply.github.com>
2021-04-05 23:48:46 +0530

    Move test questions fetching to Test class

--- app/app/Http/Controllers/QuestionController.php
+++ app/app/Http/Controllers/QuestionController.php
@@ -8,26 +8,21 @@ use App\Learner;
 use App\Test;
 use Session;
 use DB;
+use Lang;
 
 class QuestionController extends Controller
 {
     public function get_questions(Request $request){
-      
-      $questions = $questions = DB::table('questions')->
-      select('id', 'img', 'question', 'option1','option2','option3','option4')->
-      where('level', 'E')->inRandomOrder()->limit(20)->
-      get()->all();
 
-      $test = Test::createMockTest($questions);
+      $test = Test::createMockTest();
 
       $request->session()->put('test', $test);
       $request->session()->save();
 
-      return view('learners.test.question', ["questions"=> $questions, 'test' => $test]);
+      return view('learners.test.question', ["questions"=> $test->getQuestions(), 'test' => $test]);
     }
 
     public function post_questions(Request $request){
-      
       if(Session::has('test') ){
        
         $test = Session::get('test');
@@ -38,36 +33,28 @@ class QuestionController extends Controller
         if(isset($request['finish'])){
           $test->setFinish();
           $request->session()->flush();
-          $percentage = $test->getScore()/$test->getCount() * 100;
-          
-            if($percentage > 74){
-              return view('learners.successful', ['msg1' => 'Comgratulations! You have obtained '.$test->getScore().'/'.$test->getCount(),
-              'msg2' => 'Woohoo']); 
-          }
-          return view('learners.failed', ['msg1' => 'Sorry! You have obtained '.$test->getScore().'/'.$test->getCount(),
-          'msg2' => 'Try again']);
+
+          $view = $test->getResultView();
+
+          $score = $test->getScore().'/'.$test->getCount();
+
+          return view('learners.'.$view, ['msg1' => Lang::get('response.'.$view.'.msg1').$score]);
         }
         return view('learners.test.question', 
           ["details" => $request->all(), "questions"=> $test->getQuestions()]);
       }
-      
 
         $types = Learner::where('aadhar_no', $request->aadhar_no)->pluck('type')->all();
         array_push($types,"ALL");   
-      
-        $questions = DB::table('questions')->
-          select('id', 'img', 'question', 'option1','option2','option3','option4')->
-          where('level', 'D')->inRandomOrder()->limit(20)->
-          whereIn('category', explode (', ',implode(", ",$types)))->get()->all();
 
-        $test = Test::createRealTest($request->aadhar_no, $questions);
+        $test = Test::createRealTest($request->aadhar_no, $types);
 
         $request->session()->forget('test');
         $request->session()->put('test', $test);
         $request->session()->save();
 
         return view('learners.test.question', 
-          ["details" => $request->all(), "questions"=> $questions, 'test' => $test]);
+          ["details" => $request->all(), "questions"=> $test->getQuestions(), 'test' => $test]);
     }
     
 }
