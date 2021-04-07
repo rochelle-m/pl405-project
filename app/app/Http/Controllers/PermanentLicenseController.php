6a58f8b1273b82735153272165b43cdfe123a28c
chetana-1929 <mca.1929@unigoa.ac.in>
2021-04-07 10:27:27 +0530

    changes

--- app/app/Http/Controllers/PermanentLicenseController.php
+++ app/app/Http/Controllers/PermanentLicenseController.php
@@ -12,30 +12,40 @@ class PermanentLicenseController extends Controller
     public function apply(Request $request)
     {
 
-    	if{
+    	{
 
 	    	$i_aadhar_no = $request['aadhar_no'];
+	    	$i_license_no = $request['license_no'];
 	    	$exists = Citizen::where('aadhar_no', $i_aadhar_no)->exists();
+	    	if($exists){
+	    		$license_no = DB::table('citizens')->
+	      			select('license_no')->
+	      			where('aadhar_no', $i_aadhar_no);
+
+	      		if($i_license_no == $license_no){
+	      			Citizen::create([
+				        'aadhar_no' => $request['aadhar_no'],
+				        'license_no' => $request['license_no'],
+				        'vehicle_type' => $request['type'],
+				         ]);
+
+			   			return view('permanent_license.exam_date');
+	      		}
+
+
+	    	}
 	    	
-	    	$i_license_no = DB::table('citizens')->
-	      	select('license_no')->
-	      	where('aadhar_no', $i_aadhar_no);
+	    	
 	      //$user = Citizen::where('adhar_no', $i_aadhar_no)->learner_license()
 	    
-	   		Citizen::create([
-	        'aadhar_no' => $request['aadhar_no'],
-	        'license_no' => $request['license_no'],
-	        'vehicle_type' => $request['type'],
-	         ]);
-
-   			return view('permanent_license.exam_date');
+	   		
    		}
 
-       else
+      
 
        {  
-	  		return view('learners.failed', ['msg1' => 'Oops! Application Failed',
-	            'msg2' => 'Kindly enter valid credentials and try again.']);
+	  		// return view('learners.failed', ['msg1' => 'Oops! Application Failed',
+	    //         'msg2' => 'Kindly enter valid credentials and try again.']);
   		}
     }
 
