a63277f300679093946153564c8b28859802a398
chetana-1929 <mca.1929@unigoa.ac.in>
2021-05-03 21:55:29 +0530

    resources

---
+++ app/resources/views/test_resources/contact.blade.php
@@ -0,0 +1,43 @@
+
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="css/resources.css">
+</head>
+<body>
+
+<h1 align="Centre">Contact Us</h1>
+
+<div class="row">
+  <div class="column"  style="border: 2px black solid">
+    <div class="content">
+    <h2>Address</h2>
+    
+    <img src="images/address2.png" alt="Stop" border=3 height=100 width=100>
+    <p>RTO Panjim <br> Address: 1, Rua de Ourém, Patto Centre, Panaji, Goa 403001</p>
+    <p>RTO Office Mapusa <br>Address: Marod, Mapusa, Goa 403507</p>
+     <p>RTO Office Ponda <br>Address: near new bus stand, Electricity Dept Staff Residential Colony, Ponda, Goa 403401</p>
+      </div>
+  </div>
+  <div class="column" style="border: 2px black solid">
+    <div class="content">
+    <h2>Phone</h2>
+    <img src="images/phone2.png" alt="Stop" border=3 height=100 width=100>
+     <p>RTO Panjim: <br> 2222908</p>
+      <p>RTO Mapusa: <br> 2243567</p>
+       <p>RTO Ponda:<br> 2427896</p>
+       <p>Website Support:<br> 9856453309</p>
+     </div>
+  </div>
+  <div class="column" style="border: 2px black solid">
+    <div class="content">
+    <h2>Email</h2>
+    <img src="images/email.jpg" alt="Stop" border=3 height=100 width=100>
+    <p>Website Support: <br> support-tran.goa@gov.in</p>
+    <p> Director of Transport: <br> dir-tran.goa@nic.in</p>
+  </div>
+  </div>
+</div>
+
+</body>
+</html>
